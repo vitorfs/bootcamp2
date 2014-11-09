320ff58f107c3b575a2098b96eb8bf2cf6b24fa9
Vitor Freitas <vitorfs@gmail.com>
2014-11-09 10:30:52 -0200

    add styles to compose button

--- woid/feed/static/js/feed.js
+++ woid/feed/static/js/feed.js
@@ -1,13 +1,22 @@
 $(function () {
 
   $(".btn-compose").click(function () {
-    $(".compose").slideDown();
+    $(".compose").slideDown(400, function () {
+      $(".compose textarea").focus();
+    });
   });
 
   $(".compose .btn-cancel").click(function () {
     $(".compose").slideUp(400, function () {
       $(".compose textarea").val("");
     });
+  });
+
+  $(".btn-post").click(function () {
+    //$("#compose-form textarea").prop("readonly", true);
+    //$(".btn-post").prop("disabled", true);
+    $(".btn-post .text").text("Posting…");
+    $(".btn-post .fa").removeClass("fa-check").addClass("fa-spinner fa-spin");
   });
 
 });
