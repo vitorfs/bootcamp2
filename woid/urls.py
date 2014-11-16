752120e1fda06893fba518c9cc9cc6888588b4c4
Vitor Freitas <vitorfs@gmail.com>
2014-11-16 11:26:12 -0200

    change stardust background

--- woid/urls.py
+++ woid/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'woid.core.views.home', name='home'),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'core/login.html'}, name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/login/'}, name='logout'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
     url(r'^feed/', include('woid.feed.urls')),
     url(r'^(?P<organization_name>[^/]+)/$', 'woid.core.views.organization', name='organization'),
     url(r'^(?P<organization_name>[^/]+)/manage/$', 'woid.core.views.manage', name='manage'),
